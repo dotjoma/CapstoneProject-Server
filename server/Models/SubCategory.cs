c2200f745cb8125edab471f5b7858cc2454ce4b6
dotjoma <bastejoma8@gmail.com>
2025-02-21 01:23:04 +0800

    Upload SubCategory.cs

---
+++ server/Models/SubCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class SubCategory
+    {
+        public int scId { get; set; }
+
+        public int catId { get; set; }
+
+        public string scName { get; set; } = string.Empty;
+    }
+}
