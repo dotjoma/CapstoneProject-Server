4c939428310c159bd793e34e5935508314186e79
dotjoma <bastejoma8@gmail.com>
2025-02-22 10:15:11 +0800

    Update PacketType.cs

--- server/Core/Network/PacketType.cs
+++ server/Core/Network/PacketType.cs
@@ -9,11 +9,15 @@ namespace server.Core.Network
     public enum PacketType
     {
         None = 0,
+
+        // Auth Packet
         Register = 1,
         RegisterResponse = 2,
         Login = 3,
         LoginResponse = 4,
         Logout = 5,
+
+        // Product Packet
         CreateProduct = 6,
         CreateProductResponse = 7,
         UpdateProduct = 8,
@@ -22,6 +26,8 @@ namespace server.Core.Network
         DeleteProductResponse = 11,
         GetProduct = 12,
         GetProductResponse = 13,
+
+        // Category Packet
         GetCategory = 14,
         GetCategoryResponse = 15,
         GetSubCategory = 16,
@@ -30,5 +36,11 @@ namespace server.Core.Network
         CreateCategoryResponse = 19,
         CreateSubCategory = 20,
         CreateSubCategoryResponse = 21,
+
+        // Unit Packet
+        GetUnit = 22,
+        GetUnitResponse = 23,
+        CreateUnit = 24,
+        CreateUnitResponse = 25,
     }
 }
