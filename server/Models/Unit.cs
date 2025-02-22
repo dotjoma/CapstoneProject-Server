e176cdaeba0356672704a7e7f3fd8d0d7b88e8a7
dotjoma <bastejoma8@gmail.com>
2025-02-22 10:16:20 +0800

    Upload Unit.cs

---
+++ server/Models/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Unit
+    {
+        public int unitId { get; set; }
+
+        public string? unitName { get; set; }
+
+        public string? unitDescription { get; set; }
+    }
+}
