3b222574d8936f1f003d65bd28677c2231aaad54
dotjoma <bastejoma8@gmail.com>
2025-02-22 10:16:04 +0800

    Upload Product.cs

---
+++ server/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+
+        public string? productName { get; set; }
+
+        public string? productDesc { get; set; }
+
+        public decimal? productPrice { get; set; }
+
+        public string? productImage { get; set; }
+    }
+}
